efa38b809f57fea7f81b628b73dbd63f9c4aaf65
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-12-18 23:27:54 +0100

    player model small change

--- dice-game/src/main/java/com/pablo/martin/fluvia/dicegame/domain/models/Player.java
+++ dice-game/src/main/java/com/pablo/martin/fluvia/dicegame/domain/models/Player.java
@@ -9,13 +9,13 @@ import java.util.List;
 @Setter
 public class Player {
 
-    //private Long playerId; //autogenerat
+    private Long playerId; //autogenerat
 
     private String username; //no repetit, si no proporcionat -> ANONIM
 
     //private LocalDateTime registerDate; //valor segons quan es crea
 
-    //private float winRate; // % d'èxit,TODO a decidir el format de com es guarda el valor i num de decimals
+    private float winRate; // % d'èxit,TODO a decidir el format de com es guarda el valor i num de decimals
 
     private List<Roll> rolls;  //col·lecció de tirades efectuades pel jugador
 
